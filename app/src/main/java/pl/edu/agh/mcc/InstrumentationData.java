ecdbb801565ca5a559abc98ecbcca7bb9b24de35
milowedo <miloszszwedo@gmail.com>
2021-01-11 23:00:55 +0100

    Storing deviceID in the local preferences storage to keep the Scope of uniqueness on point.

--- app/src/main/java/pl/edu/agh/mcc/InstrumentationData.java
+++ app/src/main/java/pl/edu/agh/mcc/InstrumentationData.java
@@ -1,6 +1,8 @@
 package pl.edu.agh.mcc;
 
+import android.app.Activity;
 import android.content.Context;
+import android.content.SharedPreferences;
 import android.os.Build;
 
 import java.util.UUID;
@@ -11,8 +13,11 @@ import pl.edu.agh.mcc.descriptors.NetworkInformation;
 import pl.edu.agh.mcc.descriptors.TaskInformation;
 import pl.edu.agh.mcc.descriptors.TimeMeasurements;
 
-// Should be stored in a local and remote database
 public class InstrumentationData {
+    private static final String ID_KEY = "DEVICE_ID";
+    private static final String MCC_DEVICE_INFORMATION_FILENAME = "mcc_deviceInformationFile";
+    private static String DEVICE_ID;
+
     public DeviceInformation deviceInformation;
     public BatteryInformation batteryInformation;
     public EXECUTION executionLocation;
@@ -28,10 +33,28 @@ public class InstrumentationData {
         this.taskInformation = new TaskInformation();
 
         this.networkInformation.updateInformation(context);
-
-        // This should be stored somewhere on the device so we could get it every time the same
-        this.deviceInformation.deviceId = UUID.randomUUID().toString();
+        this.deviceInformation.deviceId = getDeviceId(context);
         this.deviceInformation.deviceName = Build.MODEL;
+    }
+
+    private String getDeviceId(Context context) {
+        if (DEVICE_ID != null) {
+            return DEVICE_ID;
+        }
+
+        SharedPreferences settings = context.getSharedPreferences(MCC_DEVICE_INFORMATION_FILENAME, Activity.MODE_PRIVATE);
+
+        String id = settings.getString(ID_KEY, "");
+        if ("".equals(id)) {
+            id = UUID.randomUUID().toString();
+        }
+
+        SharedPreferences.Editor editor = settings.edit();
+        editor.putString(ID_KEY, id);
+        editor.apply();
+
+        DEVICE_ID = id;
+        return id;
     }
 
     public enum EXECUTION {
