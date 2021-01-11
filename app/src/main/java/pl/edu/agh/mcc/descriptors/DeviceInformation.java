b00b5402f3d5cfd6bf0d6ad284e0c966d223e132
milowedo <miloszszwedo@gmail.com>
2021-01-11 22:48:23 +0100

    Added cpu and memory to be saved as device information

--- app/src/main/java/pl/edu/agh/mcc/descriptors/DeviceInformation.java
+++ app/src/main/java/pl/edu/agh/mcc/descriptors/DeviceInformation.java
@@ -3,4 +3,13 @@ package pl.edu.agh.mcc.descriptors;
 public class DeviceInformation {
     public String deviceName;
     public String deviceId;
+
+    //Cannot be sure the number returned is the actual number of all the processors on the device.
+    public final int numAvailableProcessors = Runtime.getRuntime().availableProcessors();
+
+    //Maximum amount the JVM will try to use, if not specified - Long.MAX_VALUE
+    public final long maxMemoryBytes = Runtime.getRuntime().maxMemory();
+
+    //Amount of memory in the JVM.
+    public final long totalMemoryBytes = Runtime.getRuntime().totalMemory();
 }
